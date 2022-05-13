9dc881b0df505c0e937b78d81e163c150343ccdd
yohanesbonar <yohanesbonar@gmail.com>
2022-05-14 02:58:12 +0700

    create expandables detail transaction

--- app/screens/DetailTransaction/DetailTransaction.js
+++ app/screens/DetailTransaction/DetailTransaction.js
@@ -1,12 +1,14 @@
 import Clipboard from '@react-native-community/clipboard';
 import {NativeBaseProvider, Icon, Toast} from 'native-base';
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {formatDate, formatRupiah} from '../../components/utils/common';
 import HeaderToolbar from './../../../app/components/molecules/HeaderToolbar';
 
 const DetailTransaction = ({route, navigation}) => {
   console.log('params', route.params);
   let data = route.params.data ? route.params.data : {};
+  const [isExpanded, setIsExpanded] = useState(false);
   const handleBackButton = () => {
     navigation.goBack();
   };
@@ -25,14 +27,8 @@ const DetailTransaction = ({route, navigation}) => {
         title={'Detail Transaction'}
         onPressBack={() => handleBackButton()}
       />
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#FFF',
-          paddingHorizontal: 20,
-          paddingVertical: 20,
-        }}>
-        <View style={{flexDirection: 'row'}}>
+      <View style={styles.mainContainer}>
+        <View style={styles.containerIDtransaction}>
           <Text style={styles.textIDTransaction}>
             {'ID TRANSAKSI: #' + data.id}
           </Text>
@@ -45,7 +41,64 @@ const DetailTransaction = ({route, navigation}) => {
             />
           </TouchableOpacity>
         </View>
-        <Icon name="ios-copy" style={{color: '#384850'}} />
+        <View style={styles.containerLineGrey}></View>
+        <View style={styles.containerExpandDetail}>
+          <Text style={styles.textTransactionDetail}>DETAIL TRANSAKSI</Text>
+          <TouchableOpacity onPress={() => setIsExpanded(!isExpanded)}>
+            <Text style={styles.textOpen}>{isExpanded ? 'Tutup' : 'Buka'}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.secondContainerLineGrey}></View>
+        {isExpanded && (
+          <View style={{paddingVertical: 16}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.textSenderBank}>
+                {data.sender_bank ? data.sender_bank : null}
+              </Text>
+              <Text style={styles.textBeneficiaryBank}>
+                {data.beneficiary_bank ? ' → ' + data.beneficiary_bank : null}
+              </Text>
+            </View>
+            <View style={styles.containerSection}>
+              <View style={styles.innerLeftSection}>
+                <Text style={styles.textLabel(true)}>
+                  {data.beneficiary_name ? data.beneficiary_name : null}
+                </Text>
+                <Text style={styles.textValue}>
+                  {data.account_number ? data.account_number : null}
+                </Text>
+              </View>
+              <View style={styles.innerRightSection}>
+                <Text style={styles.textLabel(true)}>NOMINAL</Text>
+                <Text style={styles.textValue}>
+                  {formatRupiah(data.fee.toString(), 'Rp ')}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.containerSection}>
+              <View style={styles.innerLeftSection}>
+                <Text style={styles.textLabel(true)}>BERITA TRANSFER</Text>
+                <Text style={styles.textValue}>
+                  {data.remark ? data.remark : null}
+                </Text>
+              </View>
+              <View style={styles.innerRightSection}>
+                <Text style={styles.textLabel(true)}>KODE UNIK</Text>
+                <Text style={styles.textValue}>
+                  {data.unique_code ? data.unique_code : null}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.containerSection}>
+              <View style={styles.innerLeftSection}>
+                <Text style={styles.textLabel(true)}>WAKTU DIBUAT</Text>
+                <Text style={styles.textValue}>
+                  {data.created_at ? formatDate(data.created_at) : null}
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
       </View>
     </NativeBaseProvider>
   );
@@ -64,4 +117,78 @@ const styles = StyleSheet.create({
   },
   iconCopyStyle: {width: 20, height: 20, marginTop: 3},
   touchAbleStyleIconCopy: {marginLeft: 5},
+  containerLineGrey: {
+    backgroundColor: '#f9f9f9',
+    height: 2,
+    marginTop: 16,
+    marginHorizontal: -5,
+  },
+  secondContainerLineGrey: {
+    backgroundColor: '#e6e6e6',
+    height: 1.5,
+    marginHorizontal: -5,
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  containerIDtransaction: {flexDirection: 'row'},
+  containerExpandDetail: {
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    flexDirection: 'row',
+  },
+  textTransactionDetail: {
+    fontSize: 16,
+    fontWeight: '600',
+    lineHeight: 24,
+    letterSpacing: 0.15,
+    color: '#000000',
+    textTransform: 'uppercase',
+  },
+  textOpen: {
+    fontSize: 16,
+    fontWeight: '600',
+    lineHeight: 24,
+    letterSpacing: 0.15,
+    color: '#f36a3b',
+  },
+  textSenderBank: {
+    fontSize: 17,
+    fontWeight: '700',
+    lineHeight: 24,
+    letterSpacing: 0.15,
+    color: '#000000',
+    textTransform: 'uppercase',
+  },
+  textBeneficiaryBank: {
+    fontSize: 17,
+    fontWeight: '700',
+    lineHeight: 24,
+    letterSpacing: 0.15,
+    color: '#000000',
+    textTransform: 'uppercase',
+  },
+  textLabel: isUppercase => [
+    {
+      fontSize: 16,
+      fontWeight: '600',
+      lineHeight: 24,
+      letterSpacing: 0.15,
+      color: '#000000',
+      textTransform: isUppercase ? 'uppercase' : null,
+    },
+  ],
+  textValue: {
+    fontSize: 16,
+    fontWeight: '400',
+    lineHeight: 24,
+    letterSpacing: 0.15,
+    color: '#000000',
+  },
+  containerSection: {flexDirection: 'row', marginTop: 16},
+  innerLeftSection: {flex: 0.6},
+  innerRightSection: {flex: 0.4},
 });
